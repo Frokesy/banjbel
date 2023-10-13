4d2dcb2d26647ce81bd3f310dd8e6c500a478ddc
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-10-13 20:39:47 +0100

    minor animation fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const location = useLocation();
   return (
     <Container>
-      <AnimatePresence>
+      <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/pricing" element={<Pricing />} />
