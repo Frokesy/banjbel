484a3a1bd80849ef56b08eed114737a89bc8f221
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-10-15 14:18:30 +0100

    modified header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
 import MenuItems from "./MenuItems";
-import { Twirl as Hamburger } from 'hamburger-react'
+import { Twirl as Hamburger } from "hamburger-react";
 
 const Header = () => {
   const [showItems, setShowItems] = useState(false);
   return (
-    <div className="px-4 py-3 flex items-center bg-[#f6f6f6] text-[#456db4] justify-between">
-      <div className="flex items-center space-x-2">
-        <img
-          src="/assets/sample.png"
-          alt="logo"
-          className="w-[4rem] rounded-full"
-        />
-        <span className="text-[16px] uppercase font-bold">
-          banjbel driving school
-        </span>
-      </div>
+    <div className="px-2 py-3 flex items-center bg-[#f6f6f6] text-[#456db4] justify-between">
+      <img
+        src="/assets/logo.png"
+        alt="logo"
+        className="w-[5rem]"
+      />
+      <span className="text-[16px] uppercase font-bold">
+        banjbel driving school
+      </span>
       <div className="cursor-pointer">
         <Hamburger size={24} toggled={showItems} toggle={setShowItems} />
       </div>
--- src/components/layout/Title.tsx
+++ src/components/layout/Title.tsx
@@ -6,9 +6,11 @@ interface TitleProps {
 
 const Title: FC<TitleProps> = ({ content }) => {
   return (
-    <div className="flex justify-center mt-10 mb-6">
-      <div className="bg-[#456db4] px-10 py-2">
-        <h2 className="text-[20px] font-bold text-[#fff] uppercase">{content}</h2>
+    <div className="flex flex-col w-[70%] mx-auto justify-center mt-10 mb-6">
+      <div className="relative bg-[#456db4] flex justify-center py-2">
+        <h2 className="text-[20px] font-bold text-[#fff] uppercase">
+          {content}
+        </h2>
       </div>
     </div>
   );
