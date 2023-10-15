2420d72066156d2a1a3c0704ae03a93dad7ab852
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-10-15 19:59:34 +0100

    minor fixes

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -25,7 +25,7 @@ const Popup: FC<ModalProps> = ({ isOpen, closeModal }) => {
           variants={modalVariants}
           transition={{ duration: 0.3 }}
         >
-          <div className="p-4 rounded-lg h-[80vh] shadow-lg">
+          <div className="p-4 rounded-lg h-[70vh] shadow-lg">
             <div className="flex justify-end py-3">
               <button
                 onClick={closeModal}
@@ -34,7 +34,7 @@ const Popup: FC<ModalProps> = ({ isOpen, closeModal }) => {
                 <FaTimes size={24} />
               </button>
             </div>
-            <div className="h-[70%]">
+            <div className="h-[90%]">
               <img src="/slideshow/10.jpg" alt="baff" className="h-[100%]" />
             </div>
 
