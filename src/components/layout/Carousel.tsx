6c2e65a155add97d85f495ef438dcdb4809c1c96
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-11-18 02:07:20 +0100

    rewrote carousel logic

--- src/components/layout/Carousel.tsx
+++ src/components/layout/Carousel.tsx
@@ -1,30 +1,45 @@
-import Carousel from "nuka-carousel";
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-const CarouselContainer = () => {
-    const images = [
-    "/slideshow/1.jpg",
-    "/slideshow/2.jpg",
-    "/slideshow/3.jpg",
-    "/slideshow/4.jpg",
-    "/slideshow/5.jpg",
-    "/slideshow/6.jpg",
-    "/slideshow/7.jpg",
-    "/slideshow/8.jpg",
-    "/slideshow/9.jpg",
-    "/slideshow/10.jpg",
-    ]
+const CarouselContainer: React.FC = () => {
+  const images = [
+    '/slideshow/1.jpg',
+    '/slideshow/2.jpg',
+    '/slideshow/3.jpg',
+    '/slideshow/4.jpg',
+    '/slideshow/5.jpg',
+    '/slideshow/6.jpg',
+    '/slideshow/7.jpg',
+    '/slideshow/8.jpg',
+    '/slideshow/9.jpg',
+    '/slideshow/10.jpg',
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex, images.length]);
+
   return (
-    <Carousel autoplay autoplayInterval={4000} wrapAround withoutControls animation="fade">
-        {images.map((image, index) => (
-            <div key={index} className="w-full h-[350px] lg:h-[650px]">
-            <img
-                src={image}
-                alt="slideshow"
-                className="w-full h-full object-cover"
-            />
-            </div>
-        ))}
-    </Carousel>
+    <div className="relative w-full h-[350px] lg:h-[650px] overflow-hidden">
+      <AnimatePresence mode='wait'>
+        <motion.img
+          key={currentIndex}
+          src={images[currentIndex]}
+          alt="slideshow"
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        />
+      </AnimatePresence>
+    </div>
   );
 };
 
